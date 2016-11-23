20907e5cc37a164208472058123baa3def717df4
liyuewei <2847524632@qq.com>
2016-11-23 23:21:21 +0800

    Initial commit

---
+++ src/com/lyw/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lyw.demo;
+
+/**
+ * Created by Administrator on 2016/11/22 0022.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
