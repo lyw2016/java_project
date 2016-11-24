6be88345f0176153f8c3049a37c05206960f23ae
liyuewei <2847524632@qq.com>
2016-11-24 09:01:34 +0800

    InsertionSort demo

--- src/com/lyw/demo/InsertionSort.java
+++ src/com/lyw/demo/InsertionSort.java
@@ -5,4 +5,23 @@ package com.lyw.demo;
  * 插入排序
  */
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {1, -5, 3, 78, 5, 10};
+        insertionSort(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
